58c75fea175adcef9c475ebce07833e387154303
muhammadyusuf5791 <muhammadyusufabdukarimov313@gmail.com>
2025-02-18 18:58:06 +0500

    newProject

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+import 'swiper/css';
+
+// Bootstrap CSS va JS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+// Fonts va Icons
+import './assets/css/icofont.min.css';
+import './assets/css/animate.css';
+import './assets/css/style.min.css';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './home/Home.jsx';
+import Blog from './blog/Blog.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      }
+    ]
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+);
