3563a37ce1882131122f470c5a49c5e205b1bb58
muhammadyusuf5791 <muhammadyusufabdukarimov313@gmail.com>
2025-03-16 13:50:31 +0500

    newProject

---
+++ src/home/AppSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const btnText = "Sign up for Free";
+const title = "Shop Anytime, Anywhere";
+const desc = "Take shop on your any device with our app & learn all time what you want. Just download & install & start to learn"
+
+const AppSection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppSection
