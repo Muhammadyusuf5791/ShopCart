44a67ecde4c7a465c9e266df47c333b7b10580bc
muhammadyusuf5791 <muhammadyusufabdukarimov313@gmail.com>
2025-03-19 10:45:34 +0500

    newProject

--- src/home/Sponsor.jsx
+++ src/home/Sponsor.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -7,29 +7,29 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-// import required modules
-import { Autoplay } from 'swiper/modules';
+// Import required modules
+import { Autoplay, Pagination } from 'swiper/modules';
 
 const sponsorList = [
-    {
+  {
     imgUrl: "/src/assets/images/sponsor/01.png",
-    },
-    {
+  },
+  {
     imgUrl: "/src/assets/images/sponsor/02.png",
-    },
-    {
+  },
+  {
     imgUrl: "/src/assets/images/sponsor/03.png",
-    },
-    {
+  },
+  {
     imgUrl: "/src/assets/images/sponsor/04.png",
-    },
-    {
+  },
+  {
     imgUrl: "/src/assets/images/sponsor/05.png",
-    },
-    {
+  },
+  {
     imgUrl: "/src/assets/images/sponsor/06.png",
-    },
-    ];
+  },
+];
 
 const Sponsor = () => {
   return (
@@ -37,49 +37,45 @@ const Sponsor = () => {
       <div className='container'>
         <div className='section-wrapper'>
           <div className='sponsor-slider'>
-          <Swiper
-        slidesPerView={2}
-        spaceBetween={20}
-        autoplay={
-          {
-            delay: 2000;
-            disableOnInteraction: false;
-          }
-        }
-        breakpoints={{
-          640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 50,
-          },
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        {
-          sponsorList.map((val, i) => (
-            <SwiperSlide key={i}>
-              <div className='sponsor-iten'>
-                <div className='sponsor-thumb'>
-                  <img src={val.imgUrl} alt="" />
-                </div>
-              </div>
-            </SwiperSlide>
-          ))
-        }
-      </Swiper>
+            <Swiper
+              slidesPerView={2}
+              spaceBetween={20}
+              autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 50,
+                },
+              }}
+              modules={[Autoplay, Pagination]} // Add Pagination if needed
+              className="mySwiper"
+            >
+              {sponsorList.map((val, i) => (
+                <SwiperSlide key={i}>
+                  <div className='sponsor-item'>
+                    <div className='sponsor-thumb'>
+                      <img src={val.imgUrl} alt={`Sponsor ${i + 1}`} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sponsor
+export default Sponsor;
